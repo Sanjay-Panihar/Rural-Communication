2b33c7646c917eccbff77a3f5ecddeb099256c9c
Sanjay-Panihar <sanjaypanihar@gmail.com>
2022-08-09 00:08:09 +0530

    website template added

---
+++ resources/views/include/header-top.blade.php
@@ -0,0 +1,39 @@
+<div  class="header-top">
+    <div class="container">
+        <div class="row col-det">
+            <div class="col-lg-6 d-none d-lg-block">
+                <ul class="ulleft">
+                    <li>
+                        <i class="far fa-envelope"></i>
+                        support@ruralcommunication.com
+                        <span>|</span></li>
+                    <li>
+                        <i class="fas fa-phone-volume"></i>
+                        +876 987 666 5433</li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 folouws">
+
+                <ul class="ulright">
+                   <li> <small>Folow Us </small>:</li>
+                    <li>
+                        <i class="fab fa-facebook-square"></i>
+                    </li>
+                    <li>
+                        <i class="fab fa-twitter-square"></i>
+                    </li>
+                    <li>
+                        <i class="fab fa-instagram"></i>
+                    </li>
+                    <li>
+                        <i class="fab fa-linkedin"></i>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 d-none d-md-block col-md-6 btn-bhed">
+                <button class="btn btn-sm btn-success">Join Us</button>
+                <button class="btn btn-sm btn-default">Donate</button>
+            </div>
+        </div>
+    </div>
+</div>
