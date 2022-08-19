f76756e2c0e7dcd7c857606e64f61700f48a12f6
Sanjay-Panihar <sanjaypanihar@gmail.com>
2022-08-20 00:23:07 +0530

    Add donate route and controller. Make some changes in footer for address and contact details

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DonateController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-// Route::get('/', function () {
-//     return view('layouts.main');
-// });
 Route::get('/', [HomeController::class, 'home']);
 Route::get('/aboutUs', [HomeController::class, 'aboutUs']);
 Route::get('/services', [HomeController::class, 'services']);
 Route::get('/gallery', [HomeController::class, 'gallery']);
 Route::get('/blog', [HomeController::class, 'blog']);
 Route::get('/contact_us', [HomeController::class, 'contactUs']);
+Route::get('/donate', [DonateController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
